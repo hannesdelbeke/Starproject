f63c1115370ec7bc6ac16af1442ee4eebe15ef42
Max Nilsson <t0themax@yahoo.se>
2014-10-18 22:36:48 +0200

    UI component added to the scene

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -20,15 +20,17 @@ public class UI : MonoBehaviour {
 	
 		InputManaging = GameObject.Find("GameHandlers").GetComponent<GameHandler>();
 
-		probeSizeFromPercentage = probeSize / (float)Screen.width;
-		probeSpacingFromPercentage = probeSpacing / (float)Screen.width;
+		probeSizeFromPercentage =  (float)Screen.width / probeSize;
+		probeSpacingFromPercentage =  (float)Screen.width / probeSpacing;
+		Debug.Log ("probeSizeFromPercentage: " + probeSizeFromPercentage + " probeSpacingFromPercentage: " + probeSpacingFromPercentage);
+
 	}
 	
 	void OnGUI() {
 		
 		for (int i = 0; i < InputManaging.probesAmount; i++) {
 			
-			GUI.Label (new Rect (0,0,100,50), probeGUITexture);
+			GUI.Label (new Rect ( 10 + i*50, 10, 50, 50), probeGUITexture);
 			
 			
 		}
