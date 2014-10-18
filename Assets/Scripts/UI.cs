1691db3c583a89db27fcb3ec3e2a0e6bd5b69af0
Max Nilsson <t0themax@yahoo.se>
2014-10-18 22:56:54 +0200

    UI sizing based on percentage, jam style code o_O =P

--- Assets/Scripts/UI.cs
+++ Assets/Scripts/UI.cs
@@ -9,8 +9,8 @@ public class UI : MonoBehaviour {
 	public Texture2D probeGUITexture;
 	public int probesLeftOffset = 10;
 	public int probesTopOffset = 10;
-	public float probeSize = 5f;
-	public float probeSpacing = 1f;
+	public float probeSize = 0.05f;
+	public float probeSpacing = 0.01f;
 
 	float probeSizeFromPercentage;
 	float probeSpacingFromPercentage;
@@ -20,19 +20,16 @@ public class UI : MonoBehaviour {
 	
 		InputManaging = GameObject.Find("GameHandlers").GetComponent<GameHandler>();
 
-		probeSizeFromPercentage =  (float)Screen.width / probeSize;
-		probeSpacingFromPercentage =  (float)Screen.width / probeSpacing;
-		Debug.Log ("probeSizeFromPercentage: " + probeSizeFromPercentage + " probeSpacingFromPercentage: " + probeSpacingFromPercentage);
-
+		probeSizeFromPercentage =  Screen.width * probeSize / 100;
+		probeSpacingFromPercentage =  (float)Screen.width * probeSpacing / 100;
 	}
 	
 	void OnGUI() {
 		
 		for (int i = 0; i < InputManaging.probesAmount; i++) {
 			
-			GUI.Label (new Rect ( 10 + i*50, 10, 50, 50), probeGUITexture);
-			
-			
+			GUI.Label (new Rect ( probeSpacingFromPercentage + i*probeSizeFromPercentage, probeSpacingFromPercentage/2, probeSizeFromPercentage, probeSizeFromPercentage), probeGUITexture);
+
 		}
 		
 	}
