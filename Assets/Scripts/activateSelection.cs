763ae4343a36eb3c394a12d599047805ccb463a3
Hannes Delbeke <hannesdelbeke@gmail.com>
2014-10-18 19:53:23 +0200

    d

--- Assets/Scripts/activateSelection.cs
+++ Assets/Scripts/activateSelection.cs
@@ -8,24 +8,28 @@ public class activateSelection : MonoBehaviour {
 	public float FinalScale;
 	private float workScale;
 
+	public float scaletime = 1 ;
+
 	// Use this for initialization
 	void Start () {
-
+		resetEffect ();
 	}
 
 	// Update is called once per frame
 	void Update() {		
 		//tween
 
+		//transform.localScale = new Vector3 (workScale,workScale,workScale);
 
-		transform.localScale = new Vector3 (workScale,workScale,workScale);
-		
+		Vector3 tweenScale = new Vector3 (FinalScale,FinalScale,FinalScale);
+		iTween.ScaleTo(gameObject, iTween.Hash( "scale",tweenScale, "time", scaletime, "easetype", iTween.EaseType.easeInBack));
+
 	}
 	void resetEffect (){
 	// put scale on 0
 		Vector3 startScaleVector= new Vector3 (startScale,startScale,startScale);
 		transform.localScale = startScaleVector;
-		workScale = startScale;
+		//workScale = startScale;
 
 	//put color on dark for fade
 
