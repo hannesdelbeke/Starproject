cac4655ad217cc0065a831f241eb4bf4f8339536
Max Nilsson <t0themax@yahoo.se>
2014-10-19 01:34:21 +0200

    Added MainMenu scene but no buttons yet

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public Texture mainMenuStar;
+
+	float starHeight;
+	float starWidth;
+	float starOffset;
+
+
+	// Use this for initialization
+	void Start () {
+	
+		starHeight = Screen.height * 0.8f;
+		starWidth = starHeight * 0.57014590347924f; // 508x891
+		starOffset = Screen.height * 0.1f;
+
+		Debug.Log ("starHeight: " + starHeight);
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI() {
+
+		GUI.Label (new Rect ( 0, starOffset, starWidth, starHeight), mainMenuStar);
+
+		
+	}
+
+
+
+}
