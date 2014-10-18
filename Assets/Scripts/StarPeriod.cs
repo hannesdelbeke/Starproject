4b9d152d622aa0029a1de9df3752b51b1bae6503
hjens <hjens@astro.su.se>
2014-10-18 17:22:27 +0200

    Added star period generator

---
+++ Assets/Scripts/StarPeriod.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarPeriod : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Get the orbital period of a planet, drawn from a random distribution
+	//that approximates real Kepler data
+	double GetPeriodInDays () {
+		while (true) {
+			float r1 = Random.value;
+			float r2 = Random.value*35.0;
+			float periodDistribution = 0.5 * UnityEngine.Mathf.Exp (-r2 * 0.6) - r2 * 0.0005 + 0.02;
+			if (r1 < periodDistribution) {
+				return r2;
+			}
+		}
+	}
+}
